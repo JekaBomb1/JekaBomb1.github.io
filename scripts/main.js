cd716e05011cd3f107a061f20e06e8ffc22505fd
JekaBomb1 <officer483@gmail.com>
2023-02-11 22:51:08 +0300

    update menu +, -

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,16 @@
 let tg = window.Telegram.WebApp;
+
 tg.expand();
 tg.MainButton.setText("Просмотреть заказ");
 tg.MainButton.show();
 tg.MainButton.onClick('click', function() {
 
 });
+
+let to_cart_btns = document.querySelectorAll(".tovar"); // распологать после показа всех товаров меню
+for (i = 0; i < to_cart_btns.length; i++) {
+    to_cart_btns[i].addEventListener("click", function(e) {
+        document.getElementById(e.target.id).classList.add("button_cart_plus");
+        document.getElementById(e.target.id).innerHTML = "+";
+    });
+}
--- scripts/redirect.js
+++
@@ -1,1 +0,0 @@
-window.location.href = 'https://jekabomb1.github.io/index.php';
