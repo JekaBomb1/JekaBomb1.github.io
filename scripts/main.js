339b00b244cb450dc9b3f57fe90e17df64d7ff37
JekaBomb1 <officer483@gmail.com>
2023-02-12 00:43:07 +0300

    Centering text

--- scripts/main.js
+++ scripts/main.js
@@ -12,5 +12,6 @@ for (i = 0; i < to_cart_btns.length; i++) {
     to_cart_btns[i].addEventListener("click", function(e) {
         document.getElementById(e.target.id).classList.add("button_cart_plus");
         document.getElementById(e.target.id).innerHTML = "+";
+        document.getElementById(e.target.id).classList.add("button_text_pressed");
     });
 }
